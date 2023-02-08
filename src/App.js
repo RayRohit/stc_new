9a2c417c91fd3b423a644b37616f8f8feb885c71
Rohit Ray <rohitray.kurma@navajna.com>
2023-02-08 16:26:41 +0530

    build commit'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import CarouselTry from "./CarouselTry";
 import FormPage from "./FormPage";
-import Modall from "./Modall";
 
 function App() {
   return (
     <div className="App">
       <>
         <FormPage />
-        {/* <CarouselTry /> */}
       </>
     </div>
   );
